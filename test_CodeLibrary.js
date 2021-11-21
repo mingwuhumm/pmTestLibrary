870d1632e8895000bfd470da4a373caa23d0ddba
mingwuhumm <94741508+mingwuhumm@users.noreply.github.com>
2021-11-21 22:54:57 +1300

    Update test_CodeLibrary.js
    Update the file to implementPMUtil

--- test_CodeLibrary.js
+++ test_CodeLibrary.js
@@ -1,4 +1,26 @@
-function testMessage()
-{
-    return "Hello Postman QA"
+if (typeof PMUtil === "undefined") {
+    let globalEval = eval;
+
+    PMUtil = function () {
+        let self = this;
+        const testMsg = function()
+        {
+            this.getText = () => {
+            return "Hello Postman QA"}
+        }
+    }
+    this.returnTestMsg=new testMsg();
+
+    pmutil = new PMUtil()
+
+    log = pmutil.log;
+    if (typeof init === "function") {
+        log("Running the init function")
+        init();
+    } 
+} else {
+    log("PMUtils already loaded");
+
 }
+
+
